0895e6500b28533c2edc2d4a9fc5c63430663907
Veeraldo <Veraldo24092005@gmail.com>
2026-07-16 10:10:27 +0700

    ubah password

--- reservasi-dokter/database/seeders/DatabaseSeeder.php
+++ reservasi-dokter/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
-            'password' => bcrypt('admin123'),
+            'password' => 'admin123',
             'role' => 'admin',
             'status_akun' => 'approved',
         ]);
